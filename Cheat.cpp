499ce372f4362034af6e101dbf9d747c74b89e45
Ado <adnanmaksic@u.boisestate.edu>
2025-10-23 20:06:14 -0600

    Cheetos are the second best chip.

--- Cheat.cpp
+++ Cheat.cpp
@@ -1,5 +1,7 @@
 #include "Cheat.h"
 #include "Java.h"
+#include <thread>
+#include <chrono>
 
 jclass getMinecraftVersion() {
 	return cheeto.environment->FindClass("ave");
@@ -10,8 +12,28 @@ jobject getMinecraft() {
 	return cheeto.environment->CallStaticObjectMethod(getMinecraftVersion(), getMinecraftID);
 }
 
+jobject getPlr() {
+	jfieldID getField = cheeto.environment->GetFieldID(getMinecraftVersion(), "h", "LBew;");
+	return cheeto.environment->GetObjectField(getMinecraft(), getField);
+}
+
 jobject getWorld() {
-	jfieldID getField = cheeto.environment->GetFieldID(getMinecraftVersion(), "f", "()lbdb");
+	jfieldID getField = cheeto.environment->GetFieldID(getMinecraftVersion(), "f", "Lbdb;");
+	return cheeto.environment->GetObjectField(getMinecraft(), getField);
+}
+
+//sprint in any direction
+void omnisprint() {
+	while (true) {
+		// if null then dont do notin
+		if (!!getMinecraft()) continue;
+		if (!!getPlr()) continue;
+		if (!!getWorld()) continue;
+
+
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
 }
 
 void modules() {
